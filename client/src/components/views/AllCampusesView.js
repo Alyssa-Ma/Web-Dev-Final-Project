202c6b1444bef2b9cba855bd717a6400952038f7
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-05-20 14:22:54 -0400

    linked bootstrap + css

--- client/src/components/views/AllCampusesView.js
+++ client/src/components/views/AllCampusesView.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import './css/AllCampusesView.css';
 
 const AllCampusesView = (props) => {
   if (!props.allCampuses.length) {
@@ -9,24 +10,34 @@ const AllCampusesView = (props) => {
   return (
     <div>
       {props.allCampuses.map((campus) => {
-        
-        console.log(props.allCampuses);
-        return(
-        <div key={campus.id}>
-          
-          <Link to={`/campus/${campus.id}`}>
-            <h1>{campus.name}</h1>
-          </Link>
-          <img src={campus.imageUrl} alt='campus-pic'/>
-          <p>{campus.description}</p>
-          <p>{campus.address}</p>
-          
-          
 
-        </div>
-      )
+        console.log(props.allCampuses);
+        return (
+          <div className="campus-container">
+            <div className="row">
+              <div key={campus.id} className="col-12">
+
+                <Link to={`/campus/${campus.id}`} className="campus-link">
+                  <h1>{campus.name}</h1>
+                </Link>
+              </div>
+
+              <div className="col-12">
+                <img src={campus.imageUrl} alt='campus-pic' className="campus-pic" />
+              </div>
+
+              <div className="col-12">
+                <p>{campus.description}</p>
+                <p>{campus.address}</p>
+              </div>
+
+            </div>
+          </div>
+
+        )
       })}
     </div>
+
   );
 };
 
