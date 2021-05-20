e290ed7b2fe98340d54c6dac3773c152d1dacda3
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-05-20 15:06:04 -0400

    need to update message if no students in campusview

--- client/src/components/views/CampusView.js
+++ client/src/components/views/CampusView.js
@@ -1,21 +1,34 @@
 
-
 const CampusView = (props) => {
+  
   const {campus} = props;
   //waiting for students array to be populated
   // if (campus.students === undefined){
   //   return <div>Loading...</div>
   // }
+  //no students
+
+
   return (
     <div>      
       <h1>{campus.name}</h1>
+      <img src={campus.imageUrl} alt='campus-pic'/>
+      <p>{campus.address}</p>
       <p>{campus.description}</p>
       <ul>
+      
       {campus.students.map( student => {
         let name = student.firstname + " " + student.lastname;
-        return (
-          <li key={student.id}>{name}</li>
-        );
+        if (!campus.students) {
+          console.log("no tsudents")
+          return <li>There are currently no students registered for this campus.</li>
+        }
+        else {
+          console.log("students")
+          return (
+            <li key={student.id}>{name}</li>
+          );
+        }
       })}
       </ul>
     </div>
