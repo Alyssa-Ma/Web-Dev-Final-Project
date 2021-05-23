b7ae435ed4fbf35db3dda3febf8f692127bd48aa
Joannef <joannef3188@gmail.com>
2021-05-23 01:34:09 -0400

    implement delete student

--- client/src/components/views/AddStudentView.js
+++ client/src/components/views/AddStudentView.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import './css/AddStudentView.css';
 
 class AddStudentView extends Component {
     constructor() {
@@ -7,11 +10,13 @@ class AddStudentView extends Component {
         this.state = {
             firstName: "",
             lastName: "",
-            email: ""
+            email: "",
+            gpa: ""
         };
         this.fnInput = this.fnInput.bind(this);
         this.lnInput = this.lnInput.bind(this);
         this.emailInput = this.emailInput.bind(this);
+        this.gpaInput = this.gpaInput.bind(this);
     }
 
     fnInput(event) {
@@ -25,12 +30,18 @@ class AddStudentView extends Component {
     emailInput(event) {
         this.setState({email: event.target.value});
     }
+
+    gpaInput(event) {
+        this.setState({gpa: event.target.value});
+    }
+
     async submit() {
         axios
         .post('./api/students', {
             firstname: this.state.firstName,
             lastname: this.state.lastName,
-            email: this.state.email
+            email: this.state.email,
+            gpa: this.state.gpa
         })
         .then(function(response) {
             console.log(response.data);
@@ -43,13 +54,18 @@ class AddStudentView extends Component {
     render() {
         return(
             <form>
-                <h1>Add New Student</h1>
-                First Name: <input onChange={this.fnInput}></input>
-                Last Name: <input onChange={this.lnInput}></input>
-                Email: <input onChange={this.emailInput}></input>
-                <button onClick={()=>this.submit()} variant="contained" color="primary" style={{marginTop: '20px'}}>
-                    Add Student
-                </button>
+                <h1 className="title">Add New Student</h1>
+                <div className="fill">
+                    First Name: <input onChange={this.fnInput}></input>
+                    Last Name: <input onChange={this.lnInput}></input>
+                    Email: <input onChange={this.emailInput}></input>
+                    GPA: <input onChange={this.gpaInput}></input>
+                </div>
+                <Link to={'/students'}>
+                    <Button onClick={()=>this.submit()} variant="contained" color="primary" style={{marginTop: '20px'}}>
+                        Add Student
+                    </Button>
+                </Link>
             </form>
         );
     }
--- client/src/components/views/NewCampusFormView.js
+++ client/src/components/views/NewCampusFormView.js
@@ -1,25 +1,25 @@
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
-import axios from "axios";
+import './css/NewCampusFormView.css'
 
 const NewCampusFormView = () => { 
 
     return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>New Campus Form</h1>
-            <form style={{marginTop: '20px'}}>
+        <form>
+            <h1 className="title">Add Campus</h1>
+            <div className="fill">
                 Campus Name: <input></input>
                 Campus Address: <input></input>
                 Campus Description: <input></input>
                 Campus Image: <input></input>
-                <Link to={'/campuses'}>
-                    <Button variant="contained" color="primary" style={{marginTop: '20px'}}>
-                        Add Campus
-                    </Button>
-                </Link>
-            </form>
-        </div>
+            </div>
+            <Link to={'/campuses'}>
+                <Button variant="contained" color="primary" style={{marginTop: '20px'}}>
+                    Add Campus
+                </Button>
+            </Link>
+        </form>
     );
 }
 
