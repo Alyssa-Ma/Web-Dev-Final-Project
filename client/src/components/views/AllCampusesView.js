f6deba7dcff20ebd3b730b8ae6aca25d836a8700
Steven C <stvchan999@gmail.com>
2021-05-23 02:08:24 -0400

    added delete button for campuses

--- client/src/components/views/AllCampusesView.js
+++ client/src/components/views/AllCampusesView.js
@@ -2,8 +2,21 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import './css/AllCampusesView.css';
 import Button from '@material-ui/core/Button';
+import axios from "axios";
 
 const AllCampusesView = (props) => {
+  function remove(campusID) {
+    axios
+    .delete(`/api/campuses/${campusID}`)
+    .then(function(response) {
+        console.log(response.data);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+    window.location.replace(`/campuses`);
+}
+
   if (!props.allCampuses.length) {
     return <div>There are no campuses.</div>;
   }
@@ -22,7 +35,8 @@ const AllCampusesView = (props) => {
                   <h2>{campus.name}</h2>
                 </Link>
                 
-                <img src={campus.imageUrl} alt='campus-pic' className="campus-pic" />
+                <img src={campus.imageUrl} alt='campus-pic' className="campus-pic" /><br/>
+                <Button onClick={() => remove(campus.id)} variant="contained" color="primary" style={{backgroundColor:"red"}}>DELETE</Button>
               
               </div>
             </div>
